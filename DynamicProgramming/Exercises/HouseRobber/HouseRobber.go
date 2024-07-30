2789f78063e3e6851de848f464127c9cee1ec926
Onur <hasanonurcansarihan@gmail.com>
2024-07-31 02:01:15 +0300

    new

--- DynamicProgramming/Exercises/HouseRobber/HouseRobber.go
+++ DynamicProgramming/Exercises/HouseRobber/HouseRobber.go
@@ -1,5 +1,55 @@
 package main
 
+import (
+	"fmt"
+)
+
+var input []int = []int{2, 7, 9, 3, 1}
+var houseValue []int
+var maxProfit map[int]int = make(map[int]int)
+
+func maxInt(a, b int) int {
+	if a < b {
+		return b
+	} else {
+		return a
+	}
+}
+
+func calculatePotentialValue(i int) int {
+
+	if i == 0 {
+		fmt.Println("Index is 0")
+
+		return maxProfit[0]
+	} else if i == 1 {
+		fmt.Println("Index is 1")
+		return maxProfit[1]
+	} else if _, ok := maxProfit[i]; !ok {
+		fmt.Printf("Index is  %v \n", i)
+		maxProfit[i] = maxInt(calculatePotentialValue(i-2)+houseValue[i], calculatePotentialValue(i-1))
+	}
+
+	return maxProfit[i]
+}
+
+func rob(nums []int) int {
+
+	if len(nums) >= 2 {
+		maxProfit[0] = nums[0]
+		maxProfit[1] = maxInt(nums[0], nums[1])
+		houseValue = make([]int, len(nums))
+		_ = copy(houseValue, nums)
+		calculatePotentialValue(len(nums) - 1)
+		fmt.Println(maxProfit)
+
+		return maxInt(maxProfit[len(nums)-2], maxProfit[len(nums)-1])
+	}
+	return nums[0]
+
+}
 func main() {
+
+	fmt.Println(rob(input))
 
 }
---
+++ DynamicProgramming/Exercises/MinimumCostClimbingStair/MinimumCostClimbingStair.go
@@ -0,0 +1,64 @@
+package main
+
+import "fmt"
+
+func minInt(a, b int) int {
+	if a < b {
+		return a
+	} else {
+		return b
+	}
+
+}
+
+func Climb(i int, cost []int, minCostPerStair []int) int {
+
+	// i. adım ı geçmen lazım veya i. adıma gelip tırmanman
+	minVal := 0
+	if len(cost) == 3 {
+		return minInt(cost[0]+cost[2], cost[1])
+	}
+
+	if i+1 > len(cost)-1 {
+		minVal = minInt(minCostPerStair[i-1], cost[i]+minCostPerStair[i-2])
+		return minVal
+	}
+
+	if minCostPerStair[i-1] < minCostPerStair[i-2] {
+		minVal = cost[i] + minCostPerStair[i-1]
+
+	} else {
+		minVal = cost[i] + minCostPerStair[i-2]
+	}
+
+	return minVal
+
+}
+func minCostClimbingStairs(cost []int) int {
+
+	if len(cost) == 2 {
+		return minInt(cost[0], cost[1])
+	} else if len(cost) > 2 {
+		minCostPerStair := make([]int, len(cost))
+		minCostPerStair[0] = cost[0]
+		minCostPerStair[1] = cost[1]
+
+		for i := 2; i < len(cost); i++ {
+			minCostPerStair[i] = Climb(i, cost, minCostPerStair)
+		}
+
+		fmt.Println(minCostPerStair)
+		return minCostPerStair[len(cost)-1]
+	}
+
+	return -1
+}
+
+func main() {
+	input := []int{1, 100, 1, 1, 1, 100, 1, 1, 100, 1}
+	//input := []int{10, 15, 25}
+	//input := []int{0, 0, 1, 1}
+	//input := []int{0, 1, 1, 0}
+	//input := []int{0, 1, 1, 1}
+	minCostClimbingStairs(input)
+}
